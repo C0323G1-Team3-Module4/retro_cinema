db2aaf0e2439470fea6fc93ce8f2319c85b42160
0961297922a <kimthanh130901@gmail.com>
2023-08-05 18:33:58 +0700

    [THANH][SCREENINGS]

---
+++ src/main/java/com/example/retro_cinema/screenings/repository/IScreeningsRepository.java
@@ -0,0 +1,7 @@
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              package com.example.retro_cinema.screenings.repository;
+
+import com.example.retro_cinema.screenings.model.Screenings;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IScreeningsRepository extends JpaRepository<Screenings,Integer> {
+}
---
+++ src/main/java/com/example/retro_cinema/screenings/service/IScreeningsService.java
@@ -0,0 +1,4 @@
+package com.example.retro_cinema.screenings.service;
+
+public interface IScreeningsService {
+}
