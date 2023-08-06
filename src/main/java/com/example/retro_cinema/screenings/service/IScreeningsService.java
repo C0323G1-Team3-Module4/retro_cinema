aefcf42399fdf0bcbc06b6370c68da629bd076ee
0961297922a <kimthanh130901@gmail.com>
2023-08-06 18:31:26 +0700

    [THANH][DISPLAYMOVIE-SCREENINGS]

--- src/main/java/com/example/retro_cinema/screenings/service/IScreeningsService.java
+++ src/main/java/com/example/retro_cinema/screenings/service/IScreeningsService.java
@@ -1,9 +1,12 @@
 package com.example.retro_cinema.screenings.service;
 
+import com.example.retro_cinema.screenings.dto.IScreeningsDto;
 import com.example.retro_cinema.screenings.dto.ScreeningsDto;
 
 import java.util.List;
 
 public interface IScreeningsService {
-    List<ScreeningsDto> getAll();
+    List<IScreeningsDto> getAll();
+    void addScreenings(ScreeningsDto screeningsDto);
+    List<IScreeningsDto> getAllByNameMovie(String nameMovie);
 }
