1280f120a684023c811fe9c95c635fdc23e64ce1
AnhDao19112001 <phantaanhdao@gmail.com>
2023-08-06 21:57:34 +0700

    [dao][payment]

--- src/main/java/com/example/retro_cinema/payment/controller/PaymentController.java
+++ src/main/java/com/example/retro_cinema/payment/controller/PaymentController.java
@@ -1,6 +1,7 @@
 package com.example.retro_cinema.payment.controller;
 
 import com.example.retro_cinema.customer.service.ICustomerService;
+import com.example.retro_cinema.seats.service.ISeatsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,5 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class PaymentController {
     @Autowired
     private ICustomerService iCustomerService;
+    @Autowired
+    private ISeatsService iSeatsService;
 
 }
