0528f9033f14117d71e0d931bdf714f102b30d78
0961297922a <kimthanh130901@gmail.com>
2023-08-09 19:46:32 +0700

    [THANH][SCREENINGS]

--- src/main/java/com/example/retro_cinema/screenings/controller/ScreeningsController.java
+++ src/main/java/com/example/retro_cinema/screenings/controller/ScreeningsController.java
@@ -33,57 +33,61 @@ public class ScreeningsController {
     @Autowired
     private IShowTimesService showTimesService;
 
-//    @GetMapping("/screenings/list")
-//    public String showList(Model model) {
-//        model.addAttribute("screenings", screeningsService.getAll());
-//        return "screenings/list";
-//    }
     @GetMapping("/screenings/list")
-    public String showList(@RequestParam(defaultValue = "0")int page, Model model){
-        Pageable pageable = PageRequest.of(page,1);
-        Page<Screenings> screeningsPage = screeningsService.getAllPage(pageable);
+    public String showList(@RequestParam(defaultValue = "0") int page, Model model) {
+        Pageable pageable = PageRequest.of(page, 5);
+        Page<Screenings> screeningsPage = screeningsService.getAllPage(pageable, true);
         model.addAttribute("screenings", screeningsPage);
-        return "screenings/list";
+        return "screenings/menu";
     }
 
     @GetMapping("/screenings/showAdd")
     public String showAdd(Model model) {
         model.addAttribute("screeningsDto", new ScreeningsDto());
         model.addAttribute("showTimesList", showTimesService.getAllShowTime());
-        model.addAttribute("movieList",movieService.getAllMovie());
+        model.addAttribute("movieList", movieService.getAllMovie());
         return "/screenings/add";
     }
 
     @PostMapping("/screenings/addScreenings")
-    public String add(@Valid @ModelAttribute ScreeningsDto screeningsDto, BindingResult bindingResult, Model model){
+    public String add(@Valid @ModelAttribute ScreeningsDto screeningsDto, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
         Screenings screenings = new Screenings();
-        new ScreeningsDto().validate(screeningsDto,bindingResult);
-        if(bindingResult.hasErrors()){
-            model.addAttribute("movieList",movieService.getAllMovie());
-            model.addAttribute("showTimesList",showTimesService.getAllShowTime());
-            model.addAttribute("screeningsDto",screeningsDto);
+        new ScreeningsDto().validate(screeningsDto, bindingResult);
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("movieList", movieService.getAllMovie());
+            model.addAttribute("showTimesList", showTimesService.getAllShowTime());
+            model.addAttribute("screeningsDto", screeningsDto);
             return "/screenings/add";
         }
-        BeanUtils.copyProperties(screeningsDto,screenings);
+        BeanUtils.copyProperties(screeningsDto, screenings);
+        List<Screenings> screeningsList = screeningsService.getAll();
+        for (Screenings s : screeningsList) {
+            if (s.getDateMovie().equals(screenings.getDateMovie()) && (s.getShowTimes().getId()==screenings.getShowTimes().getId())) {
+                model.addAttribute("movieList", movieService.getAllMovie());
+                model.addAttribute("showTimesList", showTimesService.getAllShowTime());
+                model.addAttribute("screeningsDto", screeningsDto);
+                redirectAttributes.addFlashAttribute("mess", "There were screenings during that time");
+                return "/screenings/add";
+            }
+        }
         screeningsService.addScreenings(screenings);
         return "redirect:/screenings/list";
     }
 
     @GetMapping("/screenings/listByNameMovie")
-    public String showListByNameMovie(@RequestParam int id,Model model){
+    public String showListByNameMovie(@RequestParam int id, Model model) {
         Movie movie = movieService.getById(id);
         List<Screenings> screeningsList = screeningsService.getAllByNameMovie(movie.getMovieName());
-        model.addAttribute("screeningsList",screeningsList);
+        model.addAttribute("screeningsList", screeningsList);
         return "screenings/listNameMovie";
     }
 
     @PostMapping("/screenings/listByNameMovie")
     public String showScreeningsByDate(@RequestParam String date, Model model) {
-        List<Screenings> screeningsList  = screeningsService.getAllByDateTime(date);
-        model.addAttribute("screeningsLists",screeningsList);
+        List<Screenings> screeningsList = screeningsService.getAllByDateTime(date);
+        model.addAttribute("screeningsLists", screeningsList);
         return "/screenings/listScreeningsByDate";
     }
-
 
 
 }
