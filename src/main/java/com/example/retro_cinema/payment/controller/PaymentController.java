f39c417162f5560a7f4c90d9087bf5ef2b2693b1
AnhDao19112001 <phantaanhdao@gmail.com>
2023-08-10 13:56:18 +0700

    [dao][swalaler rs email pass, valid]

--- src/main/java/com/example/retro_cinema/payment/controller/PaymentController.java
+++ src/main/java/com/example/retro_cinema/payment/controller/PaymentController.java
@@ -2,10 +2,16 @@ package com.example.retro_cinema.payment.controller;
 
 import com.example.retro_cinema.customer.service.ICustomerService;
 import com.example.retro_cinema.screenings.service.IScreeningsService;
+import com.example.retro_cinema.seats.model.Seats;
 import com.example.retro_cinema.seats.service.ISeatsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("payment")
@@ -16,4 +22,10 @@ public class PaymentController {
     private ISeatsService iSeatsService;
     @Autowired
     private IScreeningsService iScreeningsService;
+
+    @PostMapping("/create")
+    public String paymentCreate(@RequestParam int quantity, @RequestParam int total, @RequestParam Integer idCustomer,
+                                @SessionAttribute List<Seats> seatsList) throws UnsupportedOperationException {
+        return null;
+    }
 }
